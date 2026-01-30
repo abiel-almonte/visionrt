02123a26f986333ef70e85c75934b44eabb2a576
Abiel Almonte <almonteabiel@gmail.com>
2026-01-30 16:54:24 -0500

    Enhance Preprocessor to support Triton and CUDA processing
    - Added a flag to the Preprocessor class to choose between Triton and CUDA for frame processing.
    - Implemented separate methods for handling frame conversion using Triton and CUDA.
    - Updated the __call__ method to direct to the appropriate processing method based on the flag.

--- visionrt/preprocess.py
+++ visionrt/preprocess.py
@@ -1,8 +1,9 @@
-import numpy
 import torch
 
 import triton
 import triton.language as tl
+
+from ._visionrt import yuyv2rgb_cuda
 
 
 Y_OFFSET = tl.constexpr(16)
@@ -74,14 +75,19 @@ BLOCK_SIZE = tl.constexpr(256)
 class Preprocessor:
     def __init__(
         self,
+        use_triton: bool = False,
         mean: list = [0.485, 0.456, 0.406],
         std: list = [0.229, 0.224, 0.225],
     ) -> None:
         assert len(mean) == 3 and len(std) == 3
         self._scale = tuple(1 / (255 * std[i]) for i in range(3))
         self._offset = tuple(-mean[i] / std[i] for i in range(3))
+        self._scale_list = list(self._scale)
+        self._offset_list = list(self._scale)
 
-    def __call__(self, frame: torch.Tensor) -> torch.Tensor:
+        self._call = self._call_triton if use_triton else self._call_cuda
+
+    def _call_triton(self, frame: torch.Tensor) -> torch.Tensor:
         h, w = frame.shape[:2]
         num_pairs = (h * w) // 2
         stride = h * w
@@ -91,9 +97,30 @@ class Preprocessor:
 
         grid = ((num_pairs + BLOCK_SIZE - 1) // BLOCK_SIZE,)
         _yuyv2rgb_kernel[grid](
-            yuyv, out, stride, num_pairs,
-            *self._scale, *self._offset,
+            yuyv,
+            out,
+            stride,
+            num_pairs,
+            *self._scale,
+            *self._offset,
             BLOCK_SIZE=BLOCK_SIZE,
         )
 
         return out.view(1, 3, h, w)
+
+    def _call_cuda(self, frame: torch.Tensor) -> torch.Tensor:
+        h, w = frame.shape[:2]
+        out = yuyv2rgb_cuda(
+            frame,
+            h,
+            w,
+            self._scale_list,
+            self._offset_list,
+        )
+
+        return out.view(1, 3, h, w)
+
+    def _call(self, frame: torch.Tensor) -> torch.Tensor: ...
+
+    def __call__(self, frame: torch.Tensor) -> torch.Tensor:
+        return self._call(frame)
