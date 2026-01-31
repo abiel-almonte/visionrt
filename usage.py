925c1b71ab3a72ed04a529607f1852de66b4b01c
Abiel Almonte <almonteabiel@gmail.com>
2026-01-31 00:26:46 -0500

    Show cuda graph trick

--- usage.py
+++ usage.py
@@ -10,8 +10,10 @@ visionrt.config.verbose = True
 visionrt.config.optims.fold_conv_bn = True
 visionrt.config.cudagraphs = True
 
+
 model = (
     nn.Sequential(
+        Preprocessor(use_triton=False),  # cuda is faster
         nn.Upsample(size=(224, 224), mode="bilinear", align_corners=False),
         resnet50(weights=ResNet50_Weights.IMAGENET1K_V2),
         nn.Softmax(dim=1),
@@ -22,19 +24,16 @@ model = (
 labels = ResNet50_Weights.IMAGENET1K_V2.meta["categories"]
 
 camera = Camera("/dev/video0", deterministic=True)
-preprocessor = Preprocessor()
-
 optimized_model = visionrt.compile(model)
+
 print(optimized_model)  # snapshot of the config
 
 with torch.inference_mode():
     for frame in camera.stream():
-        image = preprocessor(frame)
-        out = optimized_model(image)
+        out = optimized_model(frame)
 
         pred_class = out.argmax(dim=1).item()
         label = labels[pred_class]
         print(f"Predicted class: {label}")
-        break
 
 print(optimized_model)  # cuda graph status should change
